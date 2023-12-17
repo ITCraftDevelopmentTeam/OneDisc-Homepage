4978216157efd6d0672521110b61ec98029f9983
This is XiaoDeng <104149371+This-is-XiaoDeng@users.noreply.github.com>
2023-12-17 09:59:01 +0800

    Update load_version.js

--- docs/public/load_version.js
+++ docs/public/load_version.js
@@ -1,13 +1,23 @@
-const API_URL = "https://api.github.com/repos/This-is-XiaoDeng/OneDisc-Build/contents/version.json";
 
 async function fetchVersion() {
-  const response = await fetch(API_URL);
+  const response = await fetch("https://itcdt.top/OneDisc/version.json");
   const data = await response.json();
-  const contentDecoded = atob(data.content);  // decode from Base64
-  const jsonData = JSON.parse(contentDecoded);
+  document.getElementById("beta-version").innerText = data.version;
+  
+  const response = await fetch("https://itcdt.top/OneDisc/artifacts.json");
+  const data = await response.json();
+  let html = "<ol>";
+  data.artifacts.forEach(function(item, index, array) {
+    html += `<li><a href="${item.url}">${item.name}</li>`;
+  })
+  html += "</ol>"
+  document.getElementById("beta-artifacts").innerText = html;
 
-  document.getElementById("beta-version").innerText = jsonData.beta;
-  document.getElementById("stable-version").innerText = jsonData.stable;
+  const response = await fetch("https://api.github.com/repos/ITCraftDevelopmentTeam/OneDisc/releases/latest");
+  const data = await response.json();
+  document.getElementById("stable-version").innerText = data.tag_name;
+
+  
 }
 
 
