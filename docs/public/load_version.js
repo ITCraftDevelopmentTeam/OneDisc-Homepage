6d16b7de4c8707e81edac3249f8bb7e369a8120f
This is XiaoDeng <104149371+This-is-XiaoDeng@users.noreply.github.com>
2023-12-17 10:11:56 +0800

    Update load_version.js

--- docs/public/load_version.js
+++ docs/public/load_version.js
@@ -5,11 +5,11 @@ async function fetchVersion() {
   
   const response = await fetch("https://itcdt.top/OneDisc/artifacts.json");
   const data = await response.json();
-  let html = "<ol>";
+  let html = "<ul>";
   data.artifacts.forEach(function(item, index, array) {
-    html += `<li><a href="${item.url}">${item.name}</li>`;
+    html += `<li><a href="${item.url}">${item.name}</a></li>`;
   })
-  html += "</ol>"
+  html += "</ul>"
   document.getElementById("beta-artifacts").innerText = html;
 
   const stable = await (await fetch("https://api.github.com/repos/ITCraftDevelopmentTeam/OneDisc/releases/latest")).json();
